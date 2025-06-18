4f7feef560883552d98853ba1605ed6569806cb2
David Bravo <114488555+DavidBAsir@users.noreply.github.com>
2025-06-18 15:28:01 +0200

    Update java.js

--- js/java.js
+++ js/java.js
@@ -106,3 +106,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  menuToggle.addEventListener('click', function() {
+    navLinks.classList.toggle('visible');
+  });
+});
